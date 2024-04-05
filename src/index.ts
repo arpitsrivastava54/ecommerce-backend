ed6d6f01a72bd853fe14c80c9d1b1230ae9b1a3b
arpit <arpitsrivastava54@gmail.com>
2024-04-05 13:10:22 +0530

    update mongo connection

--- src/index.ts
+++ src/index.ts
@@ -6,32 +6,36 @@ import bannerRouter from "./routes/banner";
 import productCategoryRouter from "./routes/productCategory";
 import contactRouter from "./routes/contact";
 import cors from "cors";
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
-
 
 const app = express();
 
 // connect db
 mongoose
  .connect(process.env.MONGO_URI || "mongodb://0.0.0.0:27017/ecommerce")
- .then(() => console.log("database connected "))
+ .then(() => {
+  console.log("database connected ");
+  app.listen(process.env.PORT, () => {
+   console.log("server started : ", process.env.PORT);
+  });
+ })
  .catch((err) =>
   console.log("something went wrong while connecting data base", err)
  );
 
- app.use(cors({
+app.use(
+ cors({
   origin: "*",
- }));
+ })
+);
 app.use(express.json());
 
 // main-website routes
-
+app.get("/", (req, res) => {
+  res.send("Working .....");
+})
 app.use("/api/v1/products", productsRoutes);
 app.use("/api/v1/banners", bannerRouter);
 app.use("/api/v1/product-category", productCategoryRouter);
 app.use("/api/v1/contact", contactRouter);
-
-app.listen(process.env.PORT, () => {
- console.log("server started : ", process.env.PORT);
-});
